80796dc6ed52458eff42752195047b41994e432e
gli <gli@SMU13041>
2024-10-31 11:34:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TimeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Time t1 = new Time(10, 47, 56);
+            Time t2 = new Time();
+
+            t1.DisplayTime();
+            t1.DisplayTime2(t1);
+            t2.DisplayTime2(t1);
+            TimeProject.Time.DisplayTime3(t1);
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeProject
+{
+    internal class Time
+    {
+        public int Hour, Minute;
+        public double Second;
+        public Time() { 
+            Hour = 0; 
+            Minute = 0; 
+            Second = 0.0;
+        }
+        public Time(int hour, int minute) {
+            this.Hour = hour;
+            this.Minute = minute;
+            this.Second = 0.0;
+        }
+        public Time(int hour, int minute, double second) { 
+            this.Hour = hour;
+            this.Minute = minute;
+            this.Second = second;
+        }
+        
+        public void DisplayTime() {
+            Console.WriteLine($"{this.Hour}:{this.Minute}:{this.Second}");
+        }
+        public void DisplayTime2(Time t) {
+            Console.WriteLine($"{t.Hour}:{t.Minute}:{t.Second}");
+        }
+        public static void DisplayTime3(Time t)
+        {
+            Console.WriteLine($"{t.Hour}:{t.Minute}:{t.Second}");
+        }
+
+
+    }
+}
