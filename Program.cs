99205450c453e5f40bd494323362b6d1a3f0f0ce
gli <gli@SMU13041>
2024-11-04 08:50:54 -0800

    11/04

--- Program.cs
+++ Program.cs
@@ -4,14 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
-            Time t1 = new Time(10, 47, 56);
-            Time t2 = new Time();
-
-            t1.DisplayTime();
-            t1.DisplayTime2(t1);
-            t2.DisplayTime2(t1);
-            TimeProject.Time.DisplayTime3(t1);
+            int num = 1234;
+            Console.WriteLine((num/100)%10);
         }
     }
 }
